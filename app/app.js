df5ffcb94616ee7acc50a58776cf6f91ea125d46
ArthurRaffles <david.marc.ashton@gmail.com>
2015-03-21 14:28:52 +0000

    removed commented code

--- app/app.js
+++ app/app.js
@@ -4,19 +4,3 @@ var heatingController =require('./heatingController');
   var app = angular.module("myapp", [])
       .service("automationService", automationService)
       .controller("heatingController", heatingController);
-
-
-// $(document).ready(function(){
-//   console.log("ready");
-//   $("#onButton").click(function(){
-//     console.log("clicky");
-//       $.ajax({url: "http://192.168.0.85/controllerOn.py", success: function(result){
-//           $("#resp").html(result);
-//       }});
-//   });
-//   $("#offButton").click(function(){
-//       $.ajax({url: "http://192.168.0.85/controllerOff.py", success: function(result){
-//           $("#resp").html(result);
-//       }});
-//   });
-// });
